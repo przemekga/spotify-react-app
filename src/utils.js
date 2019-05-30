c4bac0d5fd458a3fd92eb001ca9d24fa06a0898e
pemol117 <pemol10@gmail.com>
2019-05-31 01:39:30 +0200

    current track info in header

--- src/utils.js
+++ src/utils.js
@@ -2,4 +2,10 @@ import * as Spotify from 'spotify-web-api-js'
 
 const spotifyApi = new Spotify();
 
-export {spotifyApi}
+const transformUnix = function(millis) {
+  var minutes = Math.floor(millis / 60000);
+  var seconds = ((millis % 60000) / 1000).toFixed(0);
+  return minutes + ":" + (seconds < 10 ? '0' : '') + seconds;
+}
+
+export {spotifyApi, transformUnix}
